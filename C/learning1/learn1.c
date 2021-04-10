2f553f44e354823433fe1911dfb211c327ccd892
FierySwampshire <woodsfiery@gmail.com>
2021-04-10 00:18:18 +0000

    updated learn1.c

--- C/learning1/learn1.c
+++ C/learning1/learn1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int main(void){
     puts("hello, world!");
-    // puts(EXIT_SUCCESS);
+    // puts(EXIT_SUCCESS);//EXIT_SUCCESS == 0
     puts("line    3");
     return EXIT_SUCCESS;
 }
